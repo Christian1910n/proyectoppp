1638f59ed059e1576aaf022798243519e2149cfd
Christian1910n <christian.garaicoa.est@tecazuay.edu.ec>
2023-05-02 21:05:53 -0500

    Login normal

--- lib/home_google_sign_in.dart
+++ lib/home_google_sign_in.dart
@@ -1,65 +1,137 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:proyectoppp/auth_with_google.dart';
-import 'package:proyectoppp/model/firebase_user.dart';
+import 'package:http/http.dart' as http;
 
 class HomeGoogleSignIn extends StatefulWidget {
   @override
   State<HomeGoogleSignIn> createState() => _HomeGoogleSignInState();
 }
 
+class LoginData {
+  String usuario;
+  String contra;
+
+  LoginData({required this.usuario, required this.contra});
+
+  Map<String, dynamic> toJson() => {
+        'usuario': usuario,
+        'contra': contra,
+      };
+}
+
 class _HomeGoogleSignInState extends State<HomeGoogleSignIn> {
-  final FirebaseUser _user = FirebaseUser();
-  final AuthServiceGoogle _auth = AuthServiceGoogle();
+  late LoginData _loginData;
+  String _token = '';
+
+  Future<void> login() async {
+    try {
+      final response = await http.post(
+        Uri.parse('http://192.168.68.110:8080/api/usuarios/login'),
+        body: _loginData.toJson(),
+      );
+      if (response.statusCode == 200) {
+        final responseBody = jsonDecode(response.body);
+        _token = responseBody['token'];
+        print(_token);
+      } else {
+        print(_loginData.contra);
+      }
+    } catch (error) {
+      print(error);
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-    _user.user = _auth.user;
+    _loginData = LoginData(usuario: '', contra: '');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('LOGIN'),
+      backgroundColor: Colors.blueGrey[100],
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 90.0),
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Divider(
+                height: 180.0,
+              ),
+              SizedBox(
+                  child: ClipPath(
+                child: Image.asset(
+                  'assets/logoista.png',
+                  fit: BoxFit.cover,
+                ),
+              )),
+              const Text(
+                'PRACTICAS',
+                style: TextStyle(fontFamily: 'cursive', fontSize: 50.0),
+              ),
+              SizedBox(
+                width: 160.0,
+                height: 15.0,
+                child: Divider(color: Colors.blueGrey[600]),
+              ),
+              TextField(
+                enableInteractiveSelection: false,
+                // autofocus: true,
+                textCapitalization: TextCapitalization.characters,
+                decoration: InputDecoration(
+                    hintText: 'USER-NAME',
+                    labelText: 'User name',
+                    suffixIcon: const Icon(Icons.verified_user),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0))),
+                onSubmitted: (valor) {
+                  setState(() {
+                    _loginData.usuario = valor;
+                  });
+                },
+              ),
+              const Divider(
+                height: 18.0,
+              ),
+              TextField(
+                enableInteractiveSelection: false,
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: 'Password',
+                    labelText: 'Password',
+                    suffixIcon: const Icon(Icons.lock_outline),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0))),
+                onSubmitted: (valor) {
+                  setState(() {
+                    _loginData.contra = valor;
+                  });
+                },
+              ),
+              const Divider(
+                height: 15.0,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.all(16.0),
+                      textStyle: const TextStyle(fontSize: 20),
+                      backgroundColor: Colors.blue),
+                  onPressed: () {
+                    login();
+                  },
+                  child: const Text('Ingresar'),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
-      body: Center(
-        child: _user.uid != null ? _logged() : _login(),
-      ),
-    );
-  }
-
-  ElevatedButton _login() {
-    return ElevatedButton.icon(
-      icon: Icon(Icons.login),
-      label: Text('Inicio de Sesión'),
-      onPressed: () async {
-        await _auth.signInGoogle();
-        setState(() {
-          _user.user = _auth.user;
-        });
-      },
-    );
-  }
-
-  Column _logged() {
-    return Column(
-      children: [
-        CircleAvatar(
-          backgroundImage: NetworkImage(_user.imageUrl!),
-        ),
-        Text(_user.name!),
-        Text(_user.email!),
-        ElevatedButton.icon(
-            icon: const Icon(Icons.logout),
-            onPressed: () async {
-              await _auth.signOutGoogle();
-              setState(() {
-                _user.user = _auth.user;
-              });
-            },
-            label: const Text('Cerrar Sesión'))
-      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,17 @@ import 'package:proyectoppp/home_google_sign_in.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
