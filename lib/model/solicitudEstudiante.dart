435d4e3cb9efdad6ec8dd4b1df0ff9efb5fe79bf
Jaqueline11 <miriancutiopala123@gmail.com>
2023-05-11 02:09:58 -0500

    lista de estudiantes postulados

--- lib/model/solicitudEstudiante.dart
+++ lib/model/solicitudEstudiante.dart
@@ -14,4 +14,14 @@ class SolicitudEstudiante {
       required this.fechaEnvio,
       required this.estudiante,
       required this.convocatoria});
+
+  factory SolicitudEstudiante.fromJson(Map<String, dynamic> json) {
+    return SolicitudEstudiante(
+      id: json['id'],
+      estado: json['estado'], 
+      fechaEnvio: DateTime.parse(json['fechaEnvio']), 
+      estudiante: Estudiante.fromJson(json['estudiante']), 
+      convocatoria: Convocatoria.fromJson(json['convocatoria']));
+  }
+
 }
--- lib/screens/listaestudiantespostulados.dart
+++ lib/screens/listaestudiantespostulados.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:proyectoppp/model/convocatoria.dart'; // Importa el modelo de Estudiante
+import 'package:intl/intl.dart';
+import 'package:proyectoppp/model/convocatoria.dart';
+import 'package:proyectoppp/model/solicitudEstudiante.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-class EstudiantesPostulados extends StatelessWidget {
-  final List<Convocatoria> convocatoria; // Lista de estudiantes postulados
+class EstudiantesPostuladosScreen extends StatefulWidget {
+  final Convocatoria convocatoria;
 
-  EstudiantesPostulados(this.convocatoria);
+  EstudiantesPostuladosScreen(this.convocatoria);
+
+  @override
+  _EstudiantesPostuladosScreenState createState() =>
+      _EstudiantesPostuladosScreenState();
+}
+
+class _EstudiantesPostuladosScreenState
+    extends State<EstudiantesPostuladosScreen> {
+  List<SolicitudEstudiante> estudiantesPostulados = [];
+
+  @override
+  void initState() {
+    super.initState();
+    obtenerEstudiantesPostulados();
+  }
+
+  void obtenerEstudiantesPostulados() async {
+    final url = Uri.parse('http://192.168.1.4:8080/solicitudEstudiante/listarxconvocatoria?id=${widget.convocatoria.id}');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      print(responseData);
+      List<SolicitudEstudiante> listaEstudiantes = [];
+
+      for (final estudianteData in responseData) {
+        final estudiante = SolicitudEstudiante.fromJson(estudianteData);
+        listaEstudiantes.add(estudiante);
+      }
+
+      setState(() {
+        estudiantesPostulados = listaEstudiantes;
+      });
+    } else {
+      print('Error al obtener la lista de estudiantes postulados');
+      print('Código de respuesta: ${response.statusCode}');
+      print('Mensaje de error: ${response.body}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +56,31 @@ class EstudiantesPostulados extends StatelessWidget {
         title: Text('Estudiantes Postulados'),
       ),
       body: ListView.builder(
-        itemCount: convocatoria.length,
-        itemBuilder: (context, index) {
-          final convocatorias = convocatoria[index];
+        itemCount: estudiantesPostulados.length,
+        itemBuilder: (BuildContext context, int index) {
+          final solicitud = estudiantesPostulados[index];
+          final estudiante = solicitud.estudiante;
+          final fechaInicio = DateFormat('dd/MM/yyyy').format(estudiantesPostulados[index].fechaEnvio!);
 
-          return ListTile(
-            title: Text(convocatorias.solicitudEmpresa.toString()),
-            subtitle: Text(
-                convocatorias.solicitudEmpresa!.numPracticantes.toString()),
+          return Column(
+            children: [
+              ListTile(
+                title: Text(estudiante.carrera.nombre.toString()),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Estado: ${solicitud.estado ? 'Aprobado' : 'Pendiente'}'),
+                    Text('Cedula: ' +solicitud.estudiante.usuario.cedula.toString()),
+                    Text('Nombres: ' +solicitud.estudiante.usuario.nombre.toString()+' '+solicitud.estudiante.usuario.apellido.toString()),
+                    Text('Fecha: $fechaInicio'),
+                  ],
+                ),
+                onTap: () {
+                  // Acciones al seleccionar un estudiante postulado
+                },
+              ),
+              Divider(), // Línea divisora
+            ],
           );
         },
       ),
